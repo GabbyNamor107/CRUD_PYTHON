98dedcfe819d42b378ec24c6cf861d9907b2a0be
GabbyNamor <taehyung107gabriela@gmail.com>
2022-11-15 20:19:09 -0600

    Add CRUD_PYTHON

---
+++ diccionarios.py
@@ -0,0 +1,43 @@
+alumno = {
+    "name": "Gabbby Namor",
+    "email": "taehyung107gabriela@gmail.com",
+    "age": 19,
+    "serie_favoritas": ['The good doctor','La casa de papel']
+}
+
+json = {
+   
+    "name": "Gabbby Namor",
+    "email": "taehyung107gabriela@gmail.com",
+    "age": 19,
+    "serie_favoritas": ['The good doctor','La casa de papel']
+}
+
+json = {
+   
+    "name": "Gabbby Namor",
+    "email": "taehyung107gabriela@gmail.com",
+    "age": 19,
+    "serie_favoritas": ['The good doctor','La casa de papel']
+},
+{
+   
+    "name": "Gabbby Namor",
+    "email": "taehyung107gabriela@gmail.com",
+    "age": 19,
+    "serie_favoritas": ['The good doctor','La casa de papel']
+},
+{
+   
+    "name": "Gabbby Namor",
+    "email": "taehyung107gabriela@gmail.com",
+    "age": 19,
+    "serie_favoritas": ['The good doctor','La casa de papel']
+},
+{
+   
+    "name": "Gabbby Namor",
+    "email": "taehyung107gabriela@gmail.com",
+    "age": 19,
+    "serie_favoritas": ['The good doctor','La casa de papel']
+}
